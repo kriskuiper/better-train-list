3cc0416c1545c2caa12ff9f07ca635efaccc2a76
Kris <kriskuiper74@gmail.com>
2019-08-05 18:08:06 +0200

    feat: implement departure list in homepage

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,23 @@
 <script>
+  import departures from '../data/fake-departures'
+  import nsApiFetch from '../lib/ns-api-fetch'
+
   import AppHeader from './components/app-header/app-header.svelte'
   import AppFooter from './components/app-footer/app-footer.svelte'
-
-  export let results
+  import DepartureList from './components/departure-list/departure-list.svelte'
 </script>
 
 <style>
-   
+  .app-home {
+    flex-grow: 1;
+    padding: 1em;
+    color: #000;
+  }
 </style>
 
 <AppHeader />
 <main class="app-home">
-  <p>Here go the results</p>
+  <h2>Departing trains:</h2>
+  <DepartureList departures="{departures}" />
 </main>
 <AppFooter />
