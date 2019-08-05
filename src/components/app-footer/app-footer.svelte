2be48851e4e48ef19ae1bca0d03102f1cae4c6c2
Kris <kriskuiper74@gmail.com>
2019-08-05 18:05:53 +0200

    feat: update header and footer styling

--- src/components/app-footer/app-footer.svelte
+++ src/components/app-footer/app-footer.svelte
@@ -1,7 +1,10 @@
-<footer>
+<style>
+    .app-footer {
+        background: #333;
+        padding: 1em;
+    }
+</style>
+
+<footer class="app-footer">
     <p>An awesome footer</p>
 </footer>
-
-<style lang="scss">
-    
-</style>
--- src/components/app-header/app-header.svelte
+++ src/components/app-header/app-header.svelte
@@ -1,7 +1,14 @@
 <style lang="scss">
-
+  .app-header {
+    position: sticky;
+    top: 0;
+    background: #651fff;
+    padding: 1em;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.2);
+    color: #fff;
+  }
 </style>
 
-<header>
+<header class="app-header">
     <h1>Better train list</h1>
 </header>
