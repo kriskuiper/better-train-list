88439652a5646b346bedad5f5c062f490e97c5f3
Kris <kriskuiper74@gmail.com>
2019-10-03 09:21:14 +0200

    feat(stations-to-json): reduce api stations to 'name': 'code'

--- scripts/stations-to-json.js
+++ scripts/stations-to-json.js
@@ -9,14 +9,24 @@ const { NS_API_KEY, NS_STATIONS_URL } = process.env
 
 stationsToJson()
 
-function stationsToJson() {
+async function stationsToJson() {
   console.log('Writing stations to json...')
 
-  fetch(NS_STATIONS_URL, { headers: { 'Ocp-Apim-Subscription-Key': NS_API_KEY } })
-    .then(response => response.json())
-    .then(stations => writeJson(stations, 'stations'))
-    .catch(error => {
-      throw error
-    })
-}
+  try {
+    const response = await fetch(NS_STATIONS_URL, { headers: { 'Ocp-Apim-Subscription-Key': NS_API_KEY } })
+    const { payload } = await response.json()
 
+    const stationsData = payload.reduce((allStations, currentStation) => {
+      if (currentStation.land === 'NL') {
+        allStations[currentStation.namen.lang] = currentStation.code
+      }
+
+      return allStations
+    }, {})
+  
+    return writeJson(stationsData, 'stations')
+  }
+  catch(error) {
+    throw new Error(`Could not write stations to JSON with error: ${error}`)
+  }
+}
