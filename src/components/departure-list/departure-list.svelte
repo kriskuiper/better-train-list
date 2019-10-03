395de1560cb0ef979fef302951aee986e7c8e99b
Kris <kriskuiper74@gmail.com>
2019-10-03 19:04:23 +0200

    feat(departure-list): build out departure content

--- src/components/departure-list/departure-list.svelte
+++ src/components/departure-list/departure-list.svelte
@@ -3,13 +3,23 @@
 </script>
 
 <style>
-
+  .departures__item {
+    margin-bottom: 1rem;
+  }
 </style>
 
 <ul class="departures">
   {#each departures as departure}
-     <li class="departures__item">
-      {departure.direction}
-     </li>
+    <li class="departures__item">
+      <h3 class="departure__title">{ departure.direction }</h3>
+      <p class="departure__category">Soort: { departure.product.longCategoryName }</p>
+      <p class="departure__track">Spoor: { departure.plannedTrack }</p>
+      <p class="departure__medium-stations-title">Stopt op de volgende stations: </p>
+      <ul class="departure__medium-stations">
+        {#each departure.routeStations as station}
+          <li>{ station.mediumName }</li>
+        {/each}
+      </ul>
+    </li>
   {/each}
 </ul>
