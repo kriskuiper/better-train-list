59bb17dc4a86951d57ebaeb196e2f56e1ad02c05
Kris <kriskuiper74@gmail.com>
2019-10-07 21:24:54 +0200

    feat(app-snackvbar): create snackbar component for handling errors

---
+++ src/components/app-snackbar/app-snackbar.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { fade } from 'svelte/transition'
+  
+  export let message
+</script>
+
+<style>
+  .app-snackbar {
+    position: fixed;
+    bottom: 1rem;
+    left: 1rem;
+    right: 1rem;
+    padding: 1rem;
+    background: #333;
+    border-radius: 4px;
+    color: white;
+    font-weight: 300;
+    font-size: 0.825rem;
+    line-height: 1.4;
+  }
+</style>
+
+<div transition:fade class="app-snackbar">
+  <p>{message}</p>
+</div>
