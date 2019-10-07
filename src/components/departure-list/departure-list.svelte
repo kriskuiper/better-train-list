c854eee32eeae5c65d1deecde7e6c444c1cdbd5e
Kris <kriskuiper74@gmail.com>
2019-10-07 19:13:23 +0200

    feat(departure-list): add departure as component, make it an ordered list

--- src/components/departure-list/departure-list.svelte
+++ src/components/departure-list/departure-list.svelte
@@ -1,25 +1,11 @@
 <script>
+  import Departure from '../departure/departure.svelte'
+
   export let departures
 </script>
 
-<style>
-  .departures__item {
-    margin-bottom: 1rem;
-  }
-</style>
-
-<ul class="departures">
+<ol class="departures">
   {#each departures as departure}
-    <li class="departures__item">
-      <h3 class="departure__title">{ departure.direction }</h3>
-      <p class="departure__category">Soort: { departure.product.longCategoryName }</p>
-      <p class="departure__track">Spoor: { departure.plannedTrack }</p>
-      <p class="departure__medium-stations-title">Stopt op de volgende stations: </p>
-      <ul class="departure__medium-stations">
-        {#each departure.routeStations as station}
-          <li>{ station.mediumName }</li>
-        {/each}
-      </ul>
-    </li>
+    <Departure departure={ departure }/>
   {/each}
-</ul>
+</ol>
